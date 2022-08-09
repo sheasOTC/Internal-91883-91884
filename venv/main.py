986f791585e03139cbc4da0f1d9c07f24517b3e4
sheasOTC <sheas@stu.otc.school.nz>
2022-08-10 09:31:45 +1200

    Main Python File

--- venv/main.py
+++ venv/main.py
@@ -1,39 +1,53 @@
 import json
-import os.path
 import random
 from datetime import datetime
 
-if not os.path.isfile(
-        'passwords.json'):  # Checks if passwords.json file exists. Creates and writes a {} if file does not exist
-    with open('passwords.json', 'w') as passwords:
-        json.dump({}, passwords)
-else:
-    pass
 
+def prompt():
+    selection = input("Welcome to Passgen!\n "
+                      "To generate a password enter: Generate\n "
+                      "To store a password enter: Store\n "
+                      "To list all passwords enter: List\n "
+                      "To see details of a password enter: Inspect\n "
+                      "To delete a password enter: Delete\n"
+                      "To exit the program enter: Exit")
+    if selection.lower() == "generate":
+        use = input("What is usage for this password?\n")
+        login = input("What is the login for this password\n")
+        generate()
+        print(f"A password for {use}")
+    elif selection.lower() == "store":
 
-def generate(use, id):
+    elif selection.lower() == "list":
+
+    elif selection.lower() == "inspect":
+
+    elif selection.lower() == "remove":
+
+def generate(use, login):
     with open('passwords.json', 'r') as f:  # Gets the JSON file
         password = json.load(f)
     if use in password:
         print(f"There is already a password for {use}.\n Please enter another use.")
     else:
-        now = datetime.now()
+        now = datetime.now()  # Gets the date and time
         dateTime = now.strftime("%d/%m/%Y %H:%M:%S")
-        chars = list('~!@#$%^&*()_+`1234567890-=qwertyuiop[]\QWERTYUIOP{}|asdfghjkl;ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>?')  # List of characters used for password generation
+        chars = list(
+            '~!@#$%^&*()_+`1234567890-=qwertyuiop[]QWERTYUIOP{}|asdfghjkl;ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>?')  # List of characters used for password generation
         generated = []
-        for char in range(random.randint(8, 20)):
-            generated.append(chars[random.randint(0, 92)])
+        for char in range(random.randint(8, 20)):  # Randomly generates length of password
+            generated.append(chars[random.randint(0, 92)])  # Adds one random character to list
             joint = ''.join(generated)
         password[use] = {}
-        password[use]["Email/Username"] = id
+        password[use]["Email/Username"] = login
         password[use]["Password"] = joint
         password[use]["Date/Time"] = dateTime
     with open('passwords.json', 'w') as f:  # Gets the JSON file
-        json.dump(password, f)  # Dumps all entered content into the JSON file
+        json.dump(password, f)  # Gets the JSON file to dump information
 
 
-def store(use, id, key):
-    with open('passwords.json', 'r') as f:  # Gets the JSON file
+def store(use, login, key):
+    with open('passwords.json', 'r') as f:
         password = json.load(f)
     if use in password:
         print(f"There is already a password for {use}.\n Please enter another use.")
@@ -41,24 +55,41 @@ def store(use, id, key):
         now = datetime.now()
         dateTime = now.strftime("%d/%m/%Y %H:%M:%S")
         password[use] = {}
-        password[use]["Email/Username"] = id
+        password[use]["Email/Username"] = login
         password[use]["Password"] = key
         password[use]["Date/Time"] = dateTime
-    with open('passwords.json', 'w') as f:  # Gets the JSON file
+    with open('passwords.json', 'w') as f:  # Stores user inputted information such as email and password
         json.dump(password, f)
 
 
-
-def inspect(use):
+def inspect(use):  # When user types a usage for a password that is valid it will print each detail about it
     with open("passwords.json", 'r') as f:
         password = json.load(f)
-    print(f"The details for {use} are " + "Email/Username:" + password[use]['Email/Username'] + " Password:" + password[use]["Password"] + " Date and Time:" + password[use]["Date/Time"])
+    if use in password:
+        print(f"The details for {use} are " + "Email/Username:" + password[use]['Email/Username'] + " Password:" +
+              password[use]["Password"] + " Date and Time:" + password[use]["Date/Time"])
+    else:
+        print(f"{use} does not exist.")
 
-def listUsages():
-    with open('passwords.json', 'r') as f:  # Gets the JSON file
+
+def list_usages():  # Lists every single usage for passwords
+    with open('passwords.json', 'r') as f:
         password = json.load(f)
     for i in password:
         print(i)
+    if len(password) == 0:  # Prints out statement if there are no passwords
+        print("You have no passwords stored.")
 
 
-inspect('school')
+def delete(use):
+    with open("passwords.json", 'r') as f:
+        password = json.load(f)
+    if use in password:
+        del password[use]  # Deletes user inputed
+        with open("passwords.json", 'w') as f:
+            json.dump(password, f)
+        print(f"Deleted {use}")
+    else:
+        print(f"{use} does not exist.")
+
+
