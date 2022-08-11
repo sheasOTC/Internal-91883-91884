223dc237d248f3c13a7ab6e87f49afdacd1718da
sheasOTC <sheas@stu.otc.school.nz>
2022-08-11 12:49:33 +1200

    Main Python File

--- venv/main.py
+++ venv/main.py
@@ -1,56 +1,74 @@
 import json
+import time
 import random
 from datetime import datetime
 
 
 def prompt():
+    with open('passwords.json', 'r') as f:  # Gets the JSON file
+        password = json.load(f)
     selection = input("Welcome to Passgen!\n "
                       "To generate a password enter: Generate\n "
                       "To store a password enter: Store\n "
                       "To list all passwords enter: List\n "
                       "To see details of a password enter: Inspect\n "
                       "To delete a password enter: Delete\n"
-                      "To exit the program enter: Exit")
+                      " To exit the program enter: Exit\n")
     if selection.lower() == "generate":
         use = input("What is usage for this password?\n")
+        while use in password:
+            use = input(f"{use} is already being used for a password. Please enter another\n")
         login = input("What is the login for this password\n")
         generate(use,login)
         print(f"A password for {use} has successfully been generated")
+        prompt()
     elif selection.lower() == "store":
         use = input("What is usage for this password?\n")
+        while use in password:
+            use = input(f"{use} is already being used for a password. Please enter another\n")
         login = input("What is the login for this password\n")
-        key = input(f"What is the password for {use}?")
-        store(use,login,key)
+        key = input(f"What is the password for {use}?\n")
+        store(use, login, key)
         print(f"Password for {use} has successfully been stored")
+        prompt()
     elif selection.lower() == "list":
         list_usages()
+        prompt()
     elif selection.lower() == "inspect":
         use = input("What is usage for this password?\n")
         inspect(use)
-    elif selection.lower() == "remove":
-        use = input("What is the use of the password you want to delete?")
+        prompt()
+    elif selection.lower() == "delete":
+        use = input("What is the use of the password you want to delete? (Caps Sensitive)\n")
+        while use not in password:
+            use = input(f"{use} is not a password. Please enter correctly. (Caps Sensitive)\n")
         delete(use)
-        print(f"Successfully deleted {}")
+        print(f"Successfully deleted {use}")
+        prompt()
     elif selection.lower() == "exit":
+        print("Exiting Passgen")
+        time.sleep(0.5)
         quit()
+    else:
+        print(f"{selection} is not valid.")
+        prompt()
+
+
 def generate(use, login):
     with open('passwords.json', 'r') as f:  # Gets the JSON file
         password = json.load(f)
-    if use in password:
-        print(f"There is already a password for {use}.\n Please enter another use.")
-    else:
-        now = datetime.now()  # Gets the date and time
-        dateTime = now.strftime("%d/%m/%Y %H:%M:%S")
-        chars = list(
-            '~!@#$%^&*()_+`1234567890-=qwertyuiop[]QWERTYUIOP{}|asdfghjkl;ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>?')  # List of characters used for password generation
-        generated = []
-        for char in range(random.randint(8, 20)):  # Randomly generates length of password
-            generated.append(chars[random.randint(0, 92)])  # Adds one random character to list
-            joint = ''.join(generated)
-        password[use] = {}
-        password[use]["Email/Username"] = login
-        password[use]["Password"] = joint
-        password[use]["Date/Time"] = dateTime
+    now = datetime.now()  # Gets the date and time
+    dateTime = now.strftime("%d/%m/%Y %H:%M:%S")
+    chars = list(
+        '~!@#$%^&*()_+`1234567890-=qwertyuiop[]QWERTYUIOP{}|asdfghjkl;ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>?')  # List of characters used for password generation
+    generated = []
+    for char in range(random.randint(8, 20)):  # Randomly generates length of password
+        generated.append(chars[random.randint(0, 92)])  # Adds one random character to list
+        joint = ''.join(generated)
+    password[use] = {}
+    password[use]["Email/Username"] = login
+    password[use]["Password"] = joint
+    password[use]["Date/Time"] = dateTime
     with open('passwords.json', 'w') as f:  # Gets the JSON file
         json.dump(password, f)  # Gets the JSON file to dump information
 
@@ -58,15 +76,12 @@ def generate(use, login):
 def store(use, login, key):
     with open('passwords.json', 'r') as f:
         password = json.load(f)
-    if use in password:
-        print(f"There is already a password for {use}.\n Please enter another use.")
-    else:
-        now = datetime.now()
-        dateTime = now.strftime("%d/%m/%Y %H:%M:%S")
-        password[use] = {}
-        password[use]["Email/Username"] = login
-        password[use]["Password"] = key
-        password[use]["Date/Time"] = dateTime
+    now = datetime.now()
+    dateTime = now.strftime("%d/%m/%Y %H:%M:%S")
+    password[use] = {}
+    password[use]["Email/Username"] = login
+    password[use]["Password"] = key
+    password[use]["Date/Time"] = dateTime
     with open('passwords.json', 'w') as f:  # Stores user inputted information such as email and password
         json.dump(password, f)
 
@@ -97,8 +112,8 @@ def delete(use):
         del password[use]  # Deletes user inputed
         with open("passwords.json", 'w') as f:
             json.dump(password, f)
-        print(f"Deleted {use}")
     else:
         print(f"{use} does not exist.")
 
 
+prompt()
